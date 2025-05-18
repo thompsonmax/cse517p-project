57b64e821152c9f0b503b7adf0189ebba30595ad
unknown <thompm22@uw.edu>
2025-05-18 16:36:28 -0700

    Make lr decay rate a hyperparam and up some hyperparams

--- src/train.py
+++ src/train.py
@@ -184,7 +184,7 @@ def train_transformer(
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.5)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=hyperparams.LR_DECAY_PER_EPOCH)
 
     train_losses = [] # List to store the training losses
     dev_metrics = [] # List to store the validation metrics
