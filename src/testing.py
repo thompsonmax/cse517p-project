0d8f0a3f3630700da579a4511a6f3818f0be957e
kavya <kavyamaria@gmail.com>
2025-06-08 18:29:25 -0700

    switch data source for testing script

--- src/testing.py
+++ src/testing.py
@@ -1,50 +1,56 @@
 import pandas as pd
-from data_importer import DataImporter
-import dataloader
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from myprogram import MyModel
 from pprint import pprint
-from torchmetrics import Precision, Recall, F1Score, Accuracy
-import torch
 import random
-import string
+from datasets import load_dataset
 
 UNICODE_BMP_MAX_CODE_POINT = 65535 # U+FFFF, spans Basic Multilingual Plane
 
-def generate_test_data(data_files="common_corpus_10/subset_1_1*.parquet", records=10000, random_state=42, only_characters=False):
-    print('only characters:', only_characters)
-    common_corpus: pd.DataFrame = DataImporter.load_common_corpus(data_files=data_files)
-    common_corpus_sampled = common_corpus.sample(n=records, random_state=random_state)
-    samples = common_corpus_sampled['text'].tolist()
+def generate_test_data(data_files="papluca/language-identification", records=1000, random_state=42):
+    dataset = load_dataset(data_files, split='train[0:10000]')
+    df = dataset.to_pandas()
+    validation_dataset = df.sample(n=records, random_state=random_state)
+    samples = validation_dataset['text'].tolist()
+
+
+    characters_to_skip = [ ' ', '\n', '.', ',', '(', ')' ]
 
     x = []
     y = []
     for sample in samples:
-        sample = sample.replace('\n', ' ')
-        if only_characters:
-            translator = str.maketrans('', '', string.punctuation + string.digits)
-            sample = sample.translate(translator)
-        valid_indices = [ i for i in range(len(sample[:100])-1) if (i+1 < len(sample)) and sample[i+1] != ' ' ]
-        if len(valid_indices) == 0:
+        if len(sample) < 5:
             continue
+            
+        length = random.choice(range(5, len(sample)-1))
+        for i in range(0, 5):
+            sample_length = length + i
+            next_char = sample_length + 1
+            if next_char >= len(sample) \
+                or sample[next_char] in characters_to_skip \
+                    or sample[sample_length] in characters_to_skip:
+                continue
+            
+            input_text = sample[:sample_length + 1]
+            expected_label = sample[next_char]
 
-        length = random.choice(valid_indices)
-        x.append(sample[:length])
-        y.append(sample[length+1])
+            x.append(input_text)
+            y.append(expected_label)
+            
     return x, y
 
 if __name__ == "__main__":
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('--work_dir', help='where to load the model from', default='work')
     parser.add_argument('--test_output', help='path to write entire output', default='work/testing_output.csv')
-    parser.add_argument('--only_characters', help='remove punctuation and numbers from the test dataset', default=False, action='store_true')
     args = parser.parse_args()
 
     print('Loading model')
     model = MyModel.load(args.work_dir)
 
     print('Generating test data')
-    test_data, labels = generate_test_data(only_characters=args.only_characters)
+    test_data, labels = generate_test_data()
+
     print('Making predictions')
     pred = model.run_pred(test_data, verbose=True)
 
